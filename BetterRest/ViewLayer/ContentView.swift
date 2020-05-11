0f2b518472846c9ea04cd87df3b0167127c7a158
Josh Franco <jsfranco127@gmail.com>
2020-05-11 12:34:56 -0700

    * Replaced each VStack in our form with a Section, where the text view is the title of the section. * Replaced the “Number of cups” stepper with a Picker showing the same range of values. * Change the user interface so that it always shows their recommended bedtime using a nice and large font.

--- BetterRest/ViewLayer/ContentView.swift
+++ BetterRest/ViewLayer/ContentView.swift
@@ -24,42 +24,61 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? Date()
     }
     
+    private var predictedBedtime: String {
+        let model = SleepCalculator()
+        let components = Calendar.current.dateComponents([.hour, .minute],
+                                                         from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let min = (components.minute ?? 0) * 60
+        
+        let prediction = try? model.prediction(wake: Double(hour + min),
+                                               estimatedSleep: sleepAmount,
+                                               coffee: Double(coffeeAmonut))
+        
+        if let prediction = prediction {
+            let sleepTime = wakeUp - prediction.actualSleep
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            return "Your ideal bed time is \(formatter.string(from: sleepTime))"
+        } else {
+            return "Sorr, there was a problem calculating your bedtime..."
+        }
+    }
+    
     // MARK: - Body
     var body: some View {
         NavigationView {
-            Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    
-                    DatePicker("Please Enter a Time",
-                               selection: $wakeUp,
-                               displayedComponents: .hourAndMinute)
-                        .labelsHidden()
-                        .datePickerStyle(WheelDatePickerStyle())
-                }
-                
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of Sleep")
-                        .font(.headline)
-                    
-                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                        Text("\(sleepAmount, specifier: "%g")")
+            VStack {
+                Form {
+                    Section(header: Text("When do you want to wake up?").font(.headline)) {
+                        DatePicker("Please Enter a Time",
+                                   selection: $wakeUp,
+                                   displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                            .datePickerStyle(WheelDatePickerStyle())
                     }
-                }
-                
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
                     
-                    Stepper(value: $coffeeAmonut, in: 1...20) {
-                        if coffeeAmonut == 1 {
-                            Text("1 cup")
-                        } else {
-                            Text("\(coffeeAmonut) cups")
+                    Section(header: Text("Desired amount of Sleep").font(.headline)) {
+                        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                            Text("\(sleepAmount, specifier: "%g")")
+                        }
+                    }
+                    
+                    Section(header: Text("Daily coffee intake").font(.headline)) {
+                        Picker("\(coffeeAmonut) \(coffeeAmonut == 1 ? "cup" : "cups")", selection: $coffeeAmonut) {
+                            ForEach(0..<21) { cups in
+                                Text("\(cups) \(cups == 1 ? "cup": "cups")")
+                            }
                         }
                     }
                 }
+                
+                Spacer()
+                
+                Text("\(predictedBedtime)")
+                    .font(.headline)
+                
             }
             .navigationBarTitle("BetterRest")
             .navigationBarItems(trailing:
